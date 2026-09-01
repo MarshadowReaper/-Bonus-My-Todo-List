3df5677bbc87f2cadd74ed60c06be4869fa43574
Tajee Goode <tajeedobbins16@gmail.com>
2026-09-01 15:52:41 -0400

    Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./shared/Header.jsx";
 import Logon from "./features/Logon.jsx";
 import TodosPage from "./features/Todos/TodoList/TodosPage.jsx";
 
-import { useAuth } from "./context/AuthContext.jsx";
+import { useAuth } from "../context/AuthContext.jsx";
 
 // This is holding the authentication data
 function App() {
